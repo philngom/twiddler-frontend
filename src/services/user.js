5805402b4f8804add2a771694a2ee0a452d8e74d
Philippe Ngom <philngom@gmail.com>
2022-05-18 12:36:56 -0700

    wrote out supabase auth functions in users.js

--- src/services/user.js
+++ src/services/user.js
@@ -1,1 +1,25 @@
-import { client } from './client.js';
+import { client, checkError } from './client.js';
+
+const getUser = async () => {
+  return client.auth.user();
+};
+
+const signInUser = async (email, password) => {
+  const { user, error } = await client.auth.signIn({ email, password });
+
+  if (error) throw error;
+  console.log(user);
+  return user;
+};
+
+const signInUser = async (email, password) => {
+  const { user, error } = await client.auth.signUp({ email, password });
+
+  if (error) throw error;
+  console.log(user);
+  return user;
+};
+
+const signInUser = async (email, password) => {
+  return client.auth.signOut();
+};
