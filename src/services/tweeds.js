cc03c397301cf61f459eaf92419152e876e84aa2
Philippe Ngom <philngom@gmail.com>
2022-05-23 10:12:11 -0700

    created supabase addItem function

--- src/services/tweeds.js
+++ src/services/tweeds.js
@@ -1,1 +1,11 @@
-import { client } from './client.js';
+import { client, getUser, checkError } from './client.js';
+
+export async function addItem(item) {
+  // const user = await getUser();
+
+  const data = await client.from('tweeds').insert({
+    content: item,
+  });
+
+  return checkError(data);
+}
