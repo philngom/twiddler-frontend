83a72dc60887a636f4e53f041c756b2eb21e2cec
Philippe Ngom <philngom@gmail.com>
2022-05-23 10:04:48 -0700

    created addTweed function in tweedprovider

--- src/context/TweedProvider.js
+++ src/context/TweedProvider.js
@@ -2,12 +2,20 @@ import { createContext, useContext, useReducer } from 'react';
 
 const initialState = [];
 
-const reducer = (state, action) => {};
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'ADD':
+  }
+};
 
 const TweedContext = createContext();
 
 export const TweedProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
+
+  const addTweed = () => {
+    dispatch({ type: 'ADD', payload: { tweed } });
+  };
 
   return (
     <TweedContext.Provider value={{ state }}>{children}</TweedContext.Provider>
--- src/views/Tweeds/Tweeds.js
+++ src/views/Tweeds/Tweeds.js
@@ -1,18 +1,17 @@
 import { useUser } from '../../context/UserContext';
-import { useLocation, useHistory, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { useTweed } from '../../context/TweedProvider';
 
 export default function Tweeds() {
   const { state } = useTweed();
-  console.log('🚀 ~ file: Tweeds.js ~ line 7 ~ Tweeds ~ state', state);
 
   const handleAddTweed = () => {};
-  const location = useLocation();
+
   return (
     <>
       <Link to="/tweed">Add Tweed</Link>
       {state.map((tweed) => {
-        return <li key={tweed.id}>{}</li>;
+        return <li key={tweed.id}>{tweed.content}</li>;
       })}
     </>
   );
